a7bfff04d5f66dac1e21a9be4ebeb0f34d81ca09
iciamomstouch <85495304+iciamomstouch@users.noreply.github.com>
2021-06-27 20:50:42 +0900

    Delete java directory

--- java/com/example/service/BoardService.java
+++
@@ -1,7 +0,0 @@
-package com.example.service;
-
-import com.example.domain.BoardVO;
-
-public interface BoardService {
-	public BoardVO read(int board_bno) throws Exception;	
-}
--- java/com/example/service/TradeService.java
+++
@@ -1,8 +0,0 @@
-package com.example.service;
-
-import com.example.domain.TradeVO;
-
-public interface TradeService {
-	public TradeVO read(int trade_bno) throws Exception;
-	public void insert(TradeVO vo) throws Exception;
-}
