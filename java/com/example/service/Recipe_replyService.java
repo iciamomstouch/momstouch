70170a0bd2b863424268313574682bbceae3a05f
iciamomstouch <85495304+iciamomstouch@users.noreply.github.com>
2021-06-28 14:49:18 +0900

    Delete java directory

--- java/com/example/service/Recipe_replyService.java
+++
@@ -1,8 +0,0 @@
-package com.example.service;
-
-import com.example.domain.Recipe_replyVO;
-
-public interface Recipe_replyService {
-	public void insert(Recipe_replyVO vo) throws Exception;
-	public void delete(int recipe_rno) throws Exception;
-}
